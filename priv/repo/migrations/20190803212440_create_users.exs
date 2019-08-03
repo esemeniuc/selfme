d0d4e46bd0b25fd68b891d76ee5992f159c07b6d
KaiWen <kaiwen.zhang@creditkarma.com>
2019-08-03 15:27:23 -0700

    add id column to users to allow insertion

--- priv/repo/migrations/20190803212440_create_users.exs
+++ priv/repo/migrations/20190803212440_create_users.exs
@@ -2,14 +2,16 @@ defmodule Selfme.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table(:users, primary_key: false) do
-      add :email, :string, primary_key: true
+    create table(:users) do
+      add :email, :string, null: false
       add :pass, :string, null: false
       add :token, :string, null: false
       add :credits, :integer, null: false
 
       timestamps()
     end
+
+    create index(:users, :email, unique: true)
 
   end
 end
